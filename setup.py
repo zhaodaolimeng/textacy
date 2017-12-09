da05403d3147cf58d70333f2133e6f24af1a0d4d
Burton DeWilde <burton@chartbeat.com>
2017-12-09 14:33:52 -0600

    Add test pyx module, incorporate into setup, gitignore, manifest

--- setup.py
+++ setup.py
@@ -3,7 +3,13 @@ import os
 import sys
 
 import setuptools
-from setuptools import setup, find_packages
+from setuptools import setup, find_packages, Extension
+
+try:
+    from Cython.Distutils import build_ext
+    has_cython = True
+except ImportError:
+    has_cython = False
 
 
 INSTALL_REQUIRES = [
@@ -38,6 +44,21 @@ else:
     EXTRAS_REQUIRE[':python_version=="2.7"'] = ['backports.csv>=1.0.1']
 
 
+# don't require cython when installing from an sdist, since c files are included
+if has_cython and not os.path.exists(os.path.join(os.path.dirname(__file__), 'PKG-INFO')):
+    SETUP_REQUIRES = ['cython>=0.25']
+    CMDCLASS = {'build_ext': build_ext}
+    ext = '.pyx'
+else:
+    SETUP_REQUIRES = []
+    CMDCLASS = {}
+    ext = '.c'
+
+EXT_MODULES = [
+    Extension('textacy.foo', ['textacy/foo' + ext]),
+]
+
+
 def read_file(fname, encoding='utf-8'):
     path = os.path.join(os.path.dirname(__file__), fname)
     return io.open(path, encoding=encoding).read()
@@ -69,6 +90,9 @@ setup(
         ],
     keywords='textacy, spacy, nlp, text processing, linguistics',
     packages=find_packages(),
+    setup_requires=SETUP_REQUIRES,
     install_requires=INSTALL_REQUIRES,
     extras_require=EXTRAS_REQUIRE,
+    cmdclass=CMDCLASS,
+    ext_modules=EXT_MODULES,
     )
